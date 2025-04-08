fd2c1761bf4ccadf48ad606038eefe7ec7b3fb5a
Giovanni Flores <jojof0703@gmail.com>
2025-04-08 17:20:54 -0600

    Update AESOneRound.py

--- AESOneRound.py
+++ AESOneRound.py
@@ -1,1 +1,9 @@
-
+"""
+This program implements one round of the AES encryption algorithm. 
+It performs the following steps on a 4x4 matrix:
+1. Subbytes: subsitutue each byte using an S-box
+2. Shift rows: rotate each row by a set offset
+3. MixColumns: Mix the columns using arithmetic in galiois field
+4. AddRoundKey: XOR the state matrix with the provided round key. 
+The program will print the state of the matrix after each step. 
+"""
